e0d2c491a9dde0bd05065c084786af3692a08b81
Utilisateur <mouhamadou-moustapha.dia@ens.uvsq.fr>
2020-10-04 16:07:28 +0200

    implémentation de la classe Fraction et Main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Fraction f=new Fraction(3,2);
+		System.out.println(f.toString());
 	}
 
 }
